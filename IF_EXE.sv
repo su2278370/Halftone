531dc64cdcb3f3ad1f4ea612436c7c8c63f2c23e
su2278370 <su2278370@gmail.com>
2016-12-11 18:54:36 +0800

    Create IF_EXE.sv

---
+++ IF_EXE.sv
@@ -0,0 +1,45 @@
+`timescale 1ns/10ps
+`include "define.sv"
+
+
+module if_id(clk,
+             rst,
+         flush,
+         stall,
+             if_pc,
+             if_inst,
+             id_pc,
+             id_inst
+);
+            
+  input clk;
+  input rst;
+  input flush;
+  input stall;
+
+  input [`RegBus]if_pc;
+  input [`RegBus]if_inst;
+  
+  output logic [`RegBus]id_pc;          
+  output logic [`RegBus]id_inst;
+  
+  always_ff@(posedge clk)begin
+    if(rst==`RstEnable)begin
+      id_pc <= `ZeroWord;
+      id_inst <= `ZeroWord;
+    end
+    else if(flush==`FlushEnable)begin
+      id_pc <= `ZeroWord;
+      id_inst <= `ZeroWord;
+    end
+    else if(stall==`StallEnable)begin
+      id_pc <= `ZeroWord;
+      id_inst <= `ZeroWord;
+    end
+    else begin
+      id_pc <= if_pc;
+      id_inst <= if_inst;
+    end
+  end     
+            
+endmodule
